242431866438f1af9967c12c277a05f467ee64c9
wendesongomes <wendeson.gomes@live.com>
2022-08-14 13:36:48 -0300

    main

--- script.js
+++ script.js
@@ -8,42 +8,77 @@ const cardNumber = document.getElementById('fNumber');
 const cardNumberp = document.querySelector('.details-number-card');
 
 const cardDateMM = document.getElementById('fDay');
-const cardDateYY = document.getElementById('FMonth');
+const cardDateYY = document.getElementById('fMonth');
+const cardDatep = document.querySelector('.details-date');
 
 const cardCvc = document.getElementById('fCvc');
+const cardCvcp = document.querySelector('.details-cvc');
 
-//finish
+const finishId = document.getElementById('container-finish');
+
 const thanks = document.getElementById('thanks');
 const thanksDetails = document.getElementById('thanks-details')
 const img = document.getElementById('img-finish');
 urlImg = "images/icon-complete.svg";
 
-const erroBlank = "can't be blank";
+var erroBlank = "can't be blank";
+
+function obrigatorio(varcard, varp){
+  if(varcard.value == "" || varcard.value == null){
+    varp.innerText = erroBlank;
+    return false;
+  } else {
+    varp.innerText = "";
+    return true;
+  }
+}
 
 function confirm(){
-    if(cardName.value != ""){
-        cardNamep.innerText = erroBlank;
-        if(cardNumber.value != ""){
-          cardNumberp.innerText = erroBlank;
-        }
-    }
+  if(
+    obrigatorio(cardName, cardNamep) &
+    obrigatorio(cardNumber, cardNumberp) &
+    obrigatorio(cardDateYY, cardDatep) &
+    obrigatorio(cardDateMM, cardDatep) &
+    obrigatorio(cardCvc, cardCvcp) &
+    noChar(cardNumber, cardNumberp) &
+    noChar(cardDateYY, cardDatep) &
+    noChar(cardDateMM, cardDatep) &
+    noChar(cardCvc, cardCvcp) 
+  ) {
+    finish();
+  }
 }
 
 function finish(){
+  finishId.style = "position: relative"
   form.remove();
   img.src = urlImg;
-  btn.innerText = "Continue";
+  btn.value = "Continue";
   thanks.innerText = "Thank you!";
   thanksDetails.innerText = "We've added your card details";
 }
 
-//estudar isso
-function formats(ele,e){
-    if(ele.value.length<19){
-      ele.value= ele.value.replace(/\W/gi, '').replace(/(.{4})/g, '$1 ');
+function noChar(inputID, textID){
+  if(isNumber(inputID) == false){
+    textID.innerText = "Wrong format, numbers only";
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function isNumber(val){ 
+  if(val.value != ""){
+    val.value = val.value.replace(/\W/gi, '').replace(/(.{4})/g, '$1');
+    return /^-?\d+$/.test(val.value);
+  }
+}
+
+//ESTUDAR BAIX
+function formats(event,e){
+    if(event.value.length<19){
+      event.value = event.value.replace(/\W/gi, '').replace(/(.{4})/g, '$1 ');
       return true;
-    }else{
-      return false;
     }
   }
   
@@ -62,7 +97,6 @@ function verefication(inputID, textID, text){
         texthtml.innerHTML = input.value;
     }
 }
-
 
 function modify(){
   verefication('fCvc', 'card-cvc', '000');
